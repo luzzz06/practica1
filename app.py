6c1d87721ae42b50faefde3a8ff03746a4dc39fb
luzzz06 <24308060610075@cetis61.edu.mx>
2026-04-17 00:22:34 -0600

    actualizacion

--- app.py
+++ app.py
@@ -15,5 +15,26 @@ def agregar():
         tareas_db.append(nueva_tarea)
     return redirect(url_for('index'))
 
+@app.route('/register', methods=['GET', 'POST']) 
+def register():
+    if request.method == 'POST':
+        usuario = request.form.get('username')
+        print(f"Registro exitoso: {usuario}")
+       
+        return redirect(url_for('index')) 
+    return render_template('registro.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        correo = request.form.get('email')
+        password = request.form.get('password')
+        print(f"Login: {correo}")
+        return redirect(url_for('index')) 
+    return render_template('login.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
